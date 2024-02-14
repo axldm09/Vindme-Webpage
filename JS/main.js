e0aa2c6bf1126450f9484cbf2704108eb30d5f83
axldm09 <chrisaxl09@gmail.com>
2024-02-14 17:16:44 -0600

    Implemantion of moving divs

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,32 @@
-$(".screenshot-carousel").owlCarousel({
+(function ($) {
+  /*  $(".screenshot-carousel").owlCarousel({
     autoplay: true,
     smartSpeed: 1000,
     items: 1,
     loop: true,
     dots: true,
-});
+}); */
+
+})(jQuery);
+
+const leftDiv = document.getElementById("left-div-ID");
+const rightDiv = document.getElementById("right-div-ID");
+
+function handleScroll() {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 330) { // Adjust this value based on when you want the animation to start
+    leftDiv.style.transform = "translateX(0)";
+    rightDiv.style.opacity = 1;
+    leftDiv.style.opacity = 1;
+    rightDiv.style.transform = "translateX(0)";
+  } else {
+    leftDiv.style.transform = "translateX(-100%)";
+    rightDiv.style.opacity = 0;
+    leftDiv.style.opacity = 0;
+    rightDiv.style.transform = "translateX(100%)";
+  }
+}
+
+window.addEventListener("scroll", handleScroll);
+
